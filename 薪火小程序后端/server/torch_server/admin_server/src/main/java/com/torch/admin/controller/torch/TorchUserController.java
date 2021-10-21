b2a088619c112ff15f40904a91097adb0495ab30
CJSDCQS <49802516+CJSDCQS@users.noreply.github.com>
2021-10-22 00:33:27 +0800

    配置 redis，初步实现 redis 工具类

--- 薪火小程序后端/server/torch_server/admin_server/src/main/java/com/torch/admin/controller/torch/TorchUserController.java
+++ 薪火小程序后端/server/torch_server/admin_server/src/main/java/com/torch/admin/controller/torch/TorchUserController.java
@@ -7,6 +7,7 @@ import commonutils.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
--- 薪火小程序后端/server/torch_server/admin_server/src/main/java/com/torch/admin/entity/robot/EnterGroup.java
+++ 薪火小程序后端/server/torch_server/admin_server/src/main/java/com/torch/admin/entity/robot/EnterGroup.java
@@ -1,6 +1,5 @@
 package com.torch.admin.entity.robot;
 
-import cn.hutool.core.date.DateTime;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
