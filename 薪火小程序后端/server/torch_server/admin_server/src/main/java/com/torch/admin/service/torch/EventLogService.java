5dc5d88f585633ce4674118c27625a7eb199a1a2
CJSDCQS <49802516+CJSDCQS@users.noreply.github.com>
2021-10-26 12:38:54 +0800

    admin server 日志、协会成员接口更新

--- 薪火小程序后端/server/torch_server/admin_server/src/main/java/com/torch/admin/service/torch/EventLogService.java
+++ 薪火小程序后端/server/torch_server/admin_server/src/main/java/com/torch/admin/service/torch/EventLogService.java
@@ -7,6 +7,6 @@ import java.util.List;
 
 public interface EventLogService extends IService<EventLog> {
 
-    public List<EventLog> getEventLog(Integer page, Integer limit);
+    List<EventLog> getEventLog(Integer page, Integer limit);
 
 }
